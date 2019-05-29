488a672c88742bd5f9e71648a1a119a773f7ad8d
Jessica Wei <jessicawei9@gmail.com>
2019-05-28 23:59:45 -0700

    Panpo moves w/ idle animations too!

--- FloppyPanpo/Assets/Script/PlayerControl.cs
+++ FloppyPanpo/Assets/Script/PlayerControl.cs
@@ -20,6 +20,22 @@ public class PlayerControl : MonoBehaviour
     {
         float deltaX = Input.GetAxis("Horizontal") * walkSpeed;
         float deltaY = Input.GetAxis("Vertical") * jumpSpeed;
+        if (deltaX != 0) {
+            if (deltaX < 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else if (deltaX > 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            GetComponent<Animator>().SetBool("isWalking", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("isWalking", false);
+        }
+
         GetComponent<Rigidbody2D>().transform.Translate((deltaX), 0f, 0f);
     }
 }
