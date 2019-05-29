8c147053f9a17c24478ae6be331407b295d49774
Jessica Wei <jessicawei9@gmail.com>
2019-05-28 22:32:21 -0700

    initializing delegate system w/ player control

---
+++ FloppyPanpo/Assets/Script/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 5.0f;
+    [SerializeField] private float jumpSpeed = 5.0f;
+    private void OnEnable()
+    {
+        UpdateHandler.UpdateOccured += ControlPlayerMovement;
+    }
+
+    private void OnDisable()
+    {
+        UpdateHandler.UpdateOccured -= ControlPlayerMovement;
+    }
+
+    void ControlPlayerMovement()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * walkSpeed;
+        float deltaY = Input.GetAxis("Vertical") * jumpSpeed;
+    }
+}
---
+++ FloppyPanpo/Assets/Script/UpdateHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateHandler : MonoBehaviour
+{
+    public delegate void onUpdate();
+
+    public static event onUpdate UpdateOccured;
+
+    private void FixedUpdate()
+    {
+        if (UpdateOccured != null)
+            UpdateOccured();
+    }
+}
