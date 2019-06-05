f9ab5cd57d919510848ce4e28700c784852c012c
Jessica Wei <jessicawei9@gmail.com>
2019-06-04 20:33:16 -0700

    Testing polymorphism

---
+++ FloppyPanpo/Assets/Script/Panpo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panpo : Playable
+{
+    public override void attack() {
+    }
+    public override void special() {
+    }
+}
