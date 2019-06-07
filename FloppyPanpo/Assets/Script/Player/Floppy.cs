afaf12989d19fbc48885b915a420da7c8a0c869e
Jessica Wei <jessicawei9@gmail.com>
2019-06-06 19:25:06 -0700

    initialize Floppy code and sorted out all of the scripts

---
+++ FloppyPanpo/Assets/Script/Player/Floppy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floppy : Playable
+{
+    protected override void attack()
+    {
+    }
+    protected override void special()
+    {
+    }
+}
