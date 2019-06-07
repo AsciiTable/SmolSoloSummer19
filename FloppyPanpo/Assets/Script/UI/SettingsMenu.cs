2259d511a5906dad3f558ef9a59a821aa4218603
Jessica Wei <jessicawei9@gmail.com>
2019-06-06 21:28:37 -0700

    Setting up settings menu

---
+++ FloppyPanpo/Assets/Script/UI/SettingsMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume) {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetQuality(int index) {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    public void SetFullscreen(bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
+    }
+}
