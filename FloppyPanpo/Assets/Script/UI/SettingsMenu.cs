d84e9e2c6fd8c29156bb37cf94731c32f43eb1c6
Jessica Wei <jessicawei9@gmail.com>
2019-06-06 21:54:51 -0700

    Settings menu complete

--- FloppyPanpo/Assets/Script/UI/SettingsMenu.cs
+++ FloppyPanpo/Assets/Script/UI/SettingsMenu.cs
@@ -2,11 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+
+    private Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        string prevOption = "";
+        for (int i = 0; i < resolutions.Length; i++) {
+
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            if (i == 0)
+            {
+                prevOption = option;
+            }
+            else {
+                if (!option.Equals(prevOption)) {
+                    options.Add(option);
+                    prevOption = option;
+                }
+            }
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetVolume(float volume) {
         audioMixer.SetFloat("MasterVolume", volume);
@@ -18,5 +57,10 @@ public class SettingsMenu : MonoBehaviour
 
     public void SetFullscreen(bool isFullscreen) {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
