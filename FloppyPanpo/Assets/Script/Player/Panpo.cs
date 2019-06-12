45d86a05b8f4255f994465d8eba2d49fac559542
Jessica Wei <jessicawei9@gmail.com>
2019-06-12 11:15:12 -0700

    setting up variables

--- FloppyPanpo/Assets/Script/Player/Panpo.cs
+++ FloppyPanpo/Assets/Script/Player/Panpo.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Panpo : Playable
 {
     [SerializeField] private float moveSpeedMultiplier = 1.3f;
+    [SerializeField] private float timeSpentRunning = 4.0f;
+    private float startTime = 0f;
     protected override void passive() {
         //if()
         walkSpeed = walkSpeed * moveSpeedMultiplier;
