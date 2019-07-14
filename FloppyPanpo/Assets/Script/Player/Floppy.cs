add77c513fd7e90c3f304dd9e681ff77884e6229
Jessica Wei <jessicawei9@gmail.com>
2019-07-14 00:37:36 -0700

    set up timed variables; need to figure out how the leap should feel

--- FloppyPanpo/Assets/Script/Player/Floppy.cs
+++ FloppyPanpo/Assets/Script/Player/Floppy.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Floppy : Playable
 {
-    // SPECIAL VARIABLES
+    // SPECIAL: Inspector variables
+    [SerializeField] private float specialCD = 10.0f;
+    [SerializeField] private float chargeMaxTime = 2f;
+
+    // SPECIAL: Timer variables to start/reset/end special
+    private bool isAvalible = false;
     private bool inLeap = false;
-    private float timeHold = 0f;
+    private bool isCharging = false;
+    private float initHold = 0f;
 
     protected override void passive()
     {
@@ -15,6 +21,27 @@ public class Floppy : Playable
     // SPECIAL
     protected override void special()
     {
+        // Start charging leap
+        if (Input.GetMouseButtonDown(0) && isAvalible)
+        {
+            isAvalible = false;
+            isCharging = true;
+            initHold = Time.time;
+        }
+        // If special is on cooldown
+        else if (!isAvalible) {
+            if (Time.time - initHold > specialCD)
+            {
+                isAvalible = true;
+            }
+        }
 
+        // If charging, look for release && leap!
+        if (isCharging) {
+            if (Input.GetMouseButtonUp(0) || Time.time - initHold >= chargeMaxTime) {
+                isCharging = false;
+                // Charged Leap
+            }
+        }
     }
 }
